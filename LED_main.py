aef3dcaae32ef8d866664b5c4bd07e0e98b4c9f6
drgh0st <alexwolf1996@t-online.de>
2019-02-09 10:25:37 +0100

    Import Main logic
    Main Logic which starts the Programm

--- LED_main.py
+++ LED_main.py
@@ -5,6 +5,7 @@ import os
 import time
 from neopixel import *
 import argparse
+from IPython import Embed
 
 
 
@@ -19,9 +20,28 @@ LED_CHANNEL = 0
 
 
 def lightup(strip, color, wait_ms):
+	#LED after LED lights up
 	for i in range(strip.numPixels()):
 		strip.setPixelColor(i, color)
 		strip.show()
 		time.sleep(1)
 
 
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+ 
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    embed()
+ 
+      
+
+
+ 
