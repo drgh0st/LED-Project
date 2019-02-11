b000202da081d389f9ca10676a8c49bf8218ec95
drgh0st <alexwolf1996@t-online.de>
2019-02-11 21:54:08 +0100

    Correction #6

--- control.py
+++ control.py
@@ -4,7 +4,7 @@ from helper import assist_functions as af
 import os
 
 
-def led_stripes_on(config):	
+def led_stripes_on(config): 
     if config[0] == 0:
         config[-1] = str(int(config[-1]) + 1)
         config[0] = 1 
@@ -13,7 +13,7 @@ def led_stripes_on(config):
         pass
 
 def led_stripes_off(config):
-	if config[0] == 1:
+    if config[0] == 1:
         config[-1] = str(int(config[-1]) + 1)
         config[0] = 0 
         write_config_file(config)
@@ -21,14 +21,14 @@ def led_stripes_off(config):
         pass
 
 def led_automatic_on(config):
-	if config[1] == 0:
+    if config[1] == 0:
         config[-1] = str(int(config[-1]) + 1)
         config[1] = 1 
         write_config_file(config)
     else:
         pass
 def led_automatic_off(config):
-	if config[1] == 1:
+    if config[1] == 1:
         config[-1] = str(int(config[-1]) + 1)
         config[1] = 0 
         write_config_file(config)
@@ -36,7 +36,7 @@ def led_automatic_off(config):
         pass
 
 def led_mode_gaming(config):
-	if config[3] == 'GAMING':
+    if config[3] == 'GAMING':
         pass
     else:
         config[-1] = str(int(config[-1]) + 1)
@@ -44,7 +44,7 @@ def led_mode_gaming(config):
         write_config_file(config)
 
 def led_mode_music(config):
-	if config[3] == 'MUSIC':
+    if config[3] == 'MUSIC':
         pass
     else:
         config[-1] = str(int(config[-1]) + 1)
@@ -52,7 +52,7 @@ def led_mode_music(config):
         write_config_file(config)
 
 def led_mode_custom(config):
-	   if config[3] == 'CUSTOM':
+       if config[3] == 'CUSTOM':
         pass
     else:
         config[-1] = str(int(config[-1]) + 1)
@@ -60,13 +60,13 @@ def led_mode_custom(config):
         write_config_file(config)
 
 def led_bright_up(config):
-	pass
+    pass
 
 def led_bright_down(config):
-	pass
+    pass
 
 def led_color_red(config):
-	if config[2] == 'RED':
+    if config[2] == 'RED':
         pass
     else:
         config[-1] = str(int(config[-1]) + 1)
@@ -74,7 +74,7 @@ def led_color_red(config):
         write_config_file(config)
 
 def led_color_green(config):
-	   if config[2] == 'GREEN':
+       if config[2] == 'GREEN':
         pass
     else:
         config[-1] = str(int(config[-1]) + 1)
@@ -82,7 +82,7 @@ def led_color_green(config):
         write_config_file(config)
 
 def led_color_blue(config):
-	   if config[2] == 'BLUE':
+       if config[2] == 'BLUE':
         pass
     else:
         config[-1] = str(int(config[-1]) + 1)
@@ -90,7 +90,7 @@ def led_color_blue(config):
         write_config_file(config)
 
 def led_pattern_idle(config):
-	if config[4] == 'IDLE':
+    if config[4] == 'IDLE':
         pass
     else:
         config[-1] = str(int(config[-1]) + 1)
@@ -127,36 +127,36 @@ if __name__ == '__main__':
     config = np.array([read_config_file()])
 
     if operation == 'lsn':
-    	led_stripes_on(config)
+        led_stripes_on(config)
     elif operation == 'lsf':
-    	led_stripes_off(config)
+        led_stripes_off(config)
     elif operation == 'lan':
-    	led_automatic_on(config)
+        led_automatic_on(config)
     elif operation == 'laf':
-    	led_automatic_off(config)
+        led_automatic_off(config)
     elif operation == 'lmg':
-    	led_mode_gaming(config)
+        led_mode_gaming(config)
     elif operation == 'lmm':
-    	led_mode_music(config)
+        led_mode_music(config)
     elif operation == 'lmc':
-    	led_mode_custom(config)
+        led_mode_custom(config)
     elif operation == 'lbu':
-    	led_bright_up(config)
+        led_bright_up(config)
     elif operation == 'lbd':
-    	led_bright_down(config):
+        led_bright_down(config):
     elif operation == 'lcr':
-    	led_color_red(config)
+        led_color_red(config)
     elif operation == 'lcg':
-    	led_color_green(config)
+        led_color_green(config)
     elif operation == 'lcb':
-    	led_color_blue(config)
+        led_color_blue(config)
     elif operation == 'lpi':
-    	led_pattern_idle(config)
+        led_pattern_idle(config)
     elif operation == 'lpw':
-    	led_pattern_wave(config)
+        led_pattern_wave(config)
     else:
-    	print('Wrong Option.')
-    	
+        print('Wrong Option.')
+        
 
 
     
